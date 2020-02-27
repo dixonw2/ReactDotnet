bd2be7af4473b1d4c4feffa0b53f7bc263c406f8
Wyatt Dixon <mrbacon2015@gmail.com>
2020-02-26 20:52:02 -0500

    Reformat AboutMusic and rename methods

--- ClientApp/src/components/AboutMusic.js
+++ ClientApp/src/components/AboutMusic.js
@@ -4,24 +4,31 @@ export class AboutMusic extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { history: null, genres: null, favorites2017: [], favorites2018: [], favorites2019: [], loading: true };
+    this.state = {
+      history: null,
+      genres: null,
+      favorites2017: [],
+      favorites2018: [],
+      favorites2019: [],
+      loading: true
+    };
   }
 
   componentDidMount() {
-    this.populateWeatherData();
+    this.populateFavoritesData();
   }
 
   static renderFavoritesTable(favorites2017, favorites2018) {
-     let itunesImg = "https://linkmaker.itunes.apple.com/embed/v1/app-icon.svg";
-     return (
+    let itunesImg = "https://linkmaker.itunes.apple.com/embed/v1/app-icon.svg";
+    return (
       <>
-        <table className='table table-striped' aria-labelledby="tabelLabel">
+        <table className='table table-striped' aria-labelledby="tableLabel">
           <thead>
             <tr>
-              <th style = {{width: "5%"}}>Number</th>
-              <th style = {{width: "45%"}}>Song</th>
-              <th style = {{width: "35%"}}>Artist</th>
-              <th style = {{width: "15%"}}>Link</th>
+              <th style={{ width: "5%" }}>Number</th>
+              <th style={{ width: "45%" }}>Song</th>
+              <th style={{ width: "35%" }}>Artist</th>
+              <th style={{ width: "15%" }}>Link</th>
             </tr>
           </thead>
           <tbody>
@@ -30,7 +37,7 @@ export class AboutMusic extends React.Component {
                 <td>{index + 1}</td>
                 <td>{song.songName}</td>
                 <td>{song.songArtist}</td>
-                <td><a href={song.songLink}><img src={itunesImg}  style={{display: "inlineBlock", overflow: "hidden", width: "40px", height: "40px"}} /></a></td>
+                <td><a href={song.songLink}><img src={itunesImg} style={{ display: "inlineBlock", overflow: "hidden", width: "40px", height: "40px" }} /></a></td>
               </tr>
             )}
           </tbody>
@@ -38,10 +45,10 @@ export class AboutMusic extends React.Component {
         <table className='table table-striped' aria-labelledby="tabelLabel">
           <thead>
             <tr>
-              <th style = {{width: "5%"}}>Number</th>
-              <th style = {{width: "45%"}}>Song</th>
-              <th style = {{width: "35%"}}>Artist</th>
-              <th style = {{width: "15%"}}>Link</th>
+              <th style={{ width: "5%" }}>Number</th>
+              <th style={{ width: "45%" }}>Song</th>
+              <th style={{ width: "35%" }}>Artist</th>
+              <th style={{ width: "15%" }}>Link</th>
             </tr>
           </thead>
           <tbody>
@@ -50,31 +57,31 @@ export class AboutMusic extends React.Component {
                 <td>{index + 1}</td>
                 <td>{song.songName}</td>
                 <td>{song.songArtist}</td>
-                <td><a href={song.songLink}><img src={itunesImg} style={{display: "inlineBlock", overflow: "hidden", width: "40px", height: "40px"}} /></a></td>
+                <td><a href={song.songLink}><img src={itunesImg} style={{ display: "inlineBlock", overflow: "hidden", width: "40px", height: "40px" }} /></a></td>
               </tr>
             )}
           </tbody>
         </table>
       </>
-          );
-        }
-      
+    );
+  }
+
   render() {
-            let contents = this.state.loading ? <p><em>Loading...</em></p> : AboutMusic.renderFavoritesTable(this.state.favorites2017, this.state.favorites2018);
-        return (
+    let contents = this.state.loading ? <p><em>Loading...</em></p> : AboutMusic.renderFavoritesTable(this.state.favorites2017, this.state.favorites2018);
+    return (
       <div>
-            <h1 id="tabelLabel" >Music</h1>
-            <p>{this.state.history}</p>
-            <p>{this.state.genres}</p>
-            {contents}
-          </div>
-          );
-        }
-      
-  async populateWeatherData() {
+        <h1 id="tableLabel" >Music</h1>
+        <p>{this.state.history}</p>
+        <p>{this.state.genres}</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async populateFavoritesData() {
     const response = await fetch('api/aboutmusic');
-          const data = await response.json();
-    this.setState({history: data.history, genres: data.genres, favorites2017: data.favorites2017, favorites2018: data.favorites2018, favorites2019: data.favorites2019, loading: false });
-        }
-      
+    const data = await response.json();
+    this.setState({ history: data.history, genres: data.genres, favorites2017: data.favorites2017, favorites2018: data.favorites2018, favorites2019: data.favorites2019, loading: false });
+  }
+
 }
